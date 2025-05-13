09c92f0268f2f803ed46bda0c0262f70d22d6dd9
woochoi <epee2000@gmail.com>
2025-05-13 19:31:45 +0900

    Update MemberType.java

--- core/src/main/java/com/member/tracking/domain/enums/MemberType.java
+++ core/src/main/java/com/member/tracking/domain/enums/MemberType.java
@@ -4,10 +4,10 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonValue;
 
 public enum MemberType {
-    PP("PP"),   // 개인1
-    EP("EP"),   // 개인2
-    IB1("IB1"), // 개인3
-    IS1("IS1"); // 개인4
+    PP("PP"),
+    EP("EP"),
+    IB1("IB1"),
+    IS1("IS1");
 
     private final String code;
 
